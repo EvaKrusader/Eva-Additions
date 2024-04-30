6a38c09628c33e833661179b5936721f1a90f32a
EvaKrusader <saver@LAPTOP-VKN8G54F.home>
2024-04-30 22:49:26 +0200

    updated modver

--- src/main/java/net/mcreator/evaadditionsforge/procedures/HcProcedure.java
+++ src/main/java/net/mcreator/evaadditionsforge/procedures/HcProcedure.java
@@ -1,14 +1,56 @@
 package net.mcreator.evaadditionsforge.procedures;
 
-import java.io.File;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nullable;
+
+import java.net.URL;
+
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.File;
+import java.io.BufferedReader;
+
+import com.google.gson.Gson;
+
+@Mod.EventBusSubscriber
 public class HcProcedure {
-	public static void execute() {
-		double ver1 = 0;
-		double ver2 = 0;
-		double ver3 = 0;
-		File file = new File("");
-		com.google.gson.JsonObject json = new com.google.gson.JsonObject();
-		String url = "";
+	@SubscribeEvent
+	public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+		execute(event, event.getEntity().level());
 	}
+
+	public static void execute(LevelAccessor world) {
+		execute(null, world);
+	}
+
+private static void execute(
+@Nullable Event event,
+LevelAccessor world
+) {
+double ver1 = 0;double ver2 = 0;double ver3 = 0;File file = new File("");com.google.gson.JsonObject json = new com.google.gson.JsonObject();String url = "";
+ver1 = 1;ver2 = 7;ver3 = 6;file = new File(System.getProperty("java.io.tmpdir"), File.separator + "modver.json");url = "https://github.com/EvaKrusader/Eva-Additions/blob/master/modver.json";{ org.apache.commons.io.FileUtils.copyURLToFile(new URL(url), testfile, 1000, 1000); } catch (IOException e) { e.printStackTrace(); }
+{
+try {
+BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+StringBuilder jsonstringbuilder = new StringBuilder();
+String line;
+while((line = bufferedReader.readLine()) != null) {
+jsonstringbuilder.append(line);
+}
+bufferedReader.close();
+json = new Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+if (json.get("ver1").getAsDouble()>ver1||json.get("ver2").getAsDouble()>ver2||json.get("ver3").getAsDouble()>ver3) {if (!world.isClientSide() && world.getServer() != null)
+world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("The version "+Math.round(json.get("ver1").getAsDouble())+"."+Math.round(json.get("ver2").getAsDouble())+"."+Math.round(json.get("ver3").getAsDouble())+" of this mod is out!")), false);}
+} catch (IOException e) {
+e.printStackTrace();
+}
+}
+}
 }
