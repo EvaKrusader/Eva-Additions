9bcdeb2be8db5535d9a70ee98be82b87b58013fa
EvaKrusader <saver@LAPTOP-VKN8G54F.home>
2024-04-30 22:57:00 +0200

    updated modver again

--- src/main/java/net/mcreator/evaadditionsforge/procedures/HcProcedure.java
+++ src/main/java/net/mcreator/evaadditionsforge/procedures/HcProcedure.java
@@ -30,27 +30,41 @@ public class HcProcedure {
 		execute(null, world);
 	}
 
-private static void execute(
-@Nullable Event event,
-LevelAccessor world
-) {
-double ver1 = 0;double ver2 = 0;double ver3 = 0;File file = new File("");com.google.gson.JsonObject json = new com.google.gson.JsonObject();String url = "";
-ver1 = 1;ver2 = 7;ver3 = 6;file = new File(System.getProperty("java.io.tmpdir"), File.separator + "modver.json");url = "https://github.com/EvaKrusader/Eva-Additions/blob/master/modver.json";{ org.apache.commons.io.FileUtils.copyURLToFile(new URL(url), testfile, 1000, 1000); } catch (IOException e) { e.printStackTrace(); }
-{
-try {
-BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-StringBuilder jsonstringbuilder = new StringBuilder();
-String line;
-while((line = bufferedReader.readLine()) != null) {
-jsonstringbuilder.append(line);
-}
-bufferedReader.close();
-json = new Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
-if (json.get("ver1").getAsDouble()>ver1||json.get("ver2").getAsDouble()>ver2||json.get("ver3").getAsDouble()>ver3) {if (!world.isClientSide() && world.getServer() != null)
-world.getServer().getPlayerList().broadcastSystemMessage(Component.literal(("The version "+Math.round(json.get("ver1").getAsDouble())+"."+Math.round(json.get("ver2").getAsDouble())+"."+Math.round(json.get("ver3").getAsDouble())+" of this mod is out!")), false);}
-} catch (IOException e) {
-e.printStackTrace();
-}
-}
-}
+	private static void execute(@Nullable Event event, LevelAccessor world) {
+		double ver1 = 0;
+		double ver2 = 0;
+		double ver3 = 0;
+		File file = new File("");
+		com.google.gson.JsonObject json = new com.google.gson.JsonObject();
+		String url = "";
+		ver1 = 1;
+		ver2 = 7;
+		ver3 = 6;
+		file = new File(System.getProperty("java.io.tmpdir"), File.separator + "modver.json");
+		url = "https://github.com/EvaKrusader/Eva-Additions/blob/master/src/main/modver.json";
+		try {
+			org.apache.commons.io.FileUtils.copyURLToFile(new URL(url), file, 1000, 1000);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		{
+			try {
+				BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+				StringBuilder jsonstringbuilder = new StringBuilder();
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					jsonstringbuilder.append(line);
+				}
+				bufferedReader.close();
+				json = new Gson().fromJson(jsonstringbuilder.toString(), com.google.gson.JsonObject.class);
+				if (json.get("ver1").getAsDouble() > ver1 || json.get("ver2").getAsDouble() > ver2 || json.get("ver3").getAsDouble() > ver3) {
+					if (!world.isClientSide() && world.getServer() != null)
+						world.getServer().getPlayerList().broadcastSystemMessage(
+								Component.literal(("The version " + Math.round(json.get("ver1").getAsDouble()) + "." + Math.round(json.get("ver2").getAsDouble()) + "." + Math.round(json.get("ver3").getAsDouble()) + " of this mod is out!")), false);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
