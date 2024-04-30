c68138340db9f5fe0ebc50febfa2912345877c9f
EvaKrusader <saver@LAPTOP-VKN8G54F.home>
2024-04-30 22:28:41 +0200

    added modver.json

---
+++ src/main/java/net/mcreator/evaadditionsforge/procedures/HcProcedure.java
@@ -0,0 +1,14 @@
+package net.mcreator.evaadditionsforge.procedures;
+
+import java.io.File;
+
+public class HcProcedure {
+	public static void execute() {
+		double ver1 = 0;
+		double ver2 = 0;
+		double ver3 = 0;
+		File file = new File("");
+		com.google.gson.JsonObject json = new com.google.gson.JsonObject();
+		String url = "";
+	}
+}
